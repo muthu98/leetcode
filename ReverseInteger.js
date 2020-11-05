a60a9b634df13a3d54591cc2770bcc79d92a52cf
muthu98 <muthukumar.mnm8@gmail.com>
2020-11-05 17:18:31 +0530

    day4

--- ReverseInteger.js
+++ ReverseInteger.js
@@ -3,9 +3,9 @@
 //  * @return {number}
 //  */
 
-Given a 32-bit signed integer, reverse digits of an integer.
+// Given a 32-bit signed integer, reverse digits of an integer.
 
-Note:
+// Note:
 // Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 // https://leetcode.com/problems/reverse-integer
 var reverse = function(x) {
